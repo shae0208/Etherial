81c9672ebec19d6945f00fc3477f8563f09d5a39
Shae Pressel <shpressel3@gmail.com>
2026-06-28 18:25:04 -0400

    fixed help function

--- cogs/help.py
+++ cogs/help.py
@@ -9,26 +9,27 @@ class HelpCog(commands.Cog):
     @app_commands.command(name='help', description='View useful information about the bot.')
     async def help(self, interaction: discord.Interaction):
         overview = (
-            "Etherial is a competitive PvP utility bot for Etheria Restart, created to help players\n"
-            "master the arena through optimized builds, matchup analysis, counter recommendations,\n"
-            "and meta-focused insights. Build for serious competitors and unions looking to gain\n"
-            "every possible advantage!"
+            "Etherial is a competitive PvP utility bot for Etheria Restart,"
+            " created to help players master the arena through optimized"
+            " builds, matchup analysis, counter recommendations, and meta-"
+            "focused insights.\n\n" 
+            "Built for serious competitors and unions looking to gain every possible advantage!"
         )
         
         commands = (
-            "/help - View useful information about the bot.",
-            "/build - View build recommendations for the selected animus.",
-            "/team - View team recommendations for the selected animus.",
-            "/counter - View counters for the selected animus.",
-            "/compare - View a comparison of two selected animus.",
-            "/speedtune - View speed tuning recommendations for the selected team.",
-            "/draft - View ban recommendations for the selected enemy draft picks."
+            "/help - View Etherial support information.\n"
+            "/build - View build guide for the selected animus.\n"
+            "/team - View team recommendations for the selected animus.\n"
+            "/counter - View counter recommendations for the selected animus.\n"
+            "/compare - View comparison of two selected animus.\n"
+            "/speedtune - View speed tuning recommendations for selected team.\n"
+            "/draft - View ban recommendations for selected enemy draft."
         )
 
         contact = (
-            "Please contact bl4ckh4wkttv.gaming@gmail.com for additional support.\n"
-            "Feel free to also provide suggestions/feedback about the bot in general,\n"
-            "or to suggest edits to inaccurate information to better the experience."
+            "Please contact bl4ckh4wkttv.gaming@gmail.com for additional support.\n\n"
+            "Feel free to also provide suggestions/feedback about the bot in general,"
+            " or to suggest edits to inaccurate information to better the experience."
         )
         
         embed = discord.Embed(title='Etherial Support', description=overview)
@@ -36,8 +37,8 @@ class HelpCog(commands.Cog):
         if self.bot.user:
             embed.set_thumbnail(url=self.bot.user.display_avatar.url)
         
-        embed.add_field(name='Commands', value=commands)
-        embed.add_field(name='Contact', value=contact)
+        embed.add_field(name='Commands', value=commands, inline=False)
+        embed.add_field(name='Contact', value=contact, inline=False)
         
         await interaction.response.send_message(embed=embed)
         
